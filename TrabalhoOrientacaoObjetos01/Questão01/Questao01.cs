19ee69b9d3db433bb83a9b98e08cb48cf13bc5fb
gustavo.prim <gustavo.prim@ACESSO08>
2022-06-01 19:15:44 -0300

    Add project files.

---
+++ TrabalhoOrientacaoObjetos01/Questão01/Questao01.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabalhoOrientacaoObjetos01.Questão01
+{
+    internal class Questao01
+    {
+    }
+}
