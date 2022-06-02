4e63ac5ae482463f9e48363168240da464698c22
marina.lima <marinalimasc@gmail.com>
2022-06-02 20:03:46 -0300

    Exercicio 1 em andamento

--- TrabalhoOrientacaoObjetos01/Questão01/ExercicioNumero.cs
+++ TrabalhoOrientacaoObjetos01/Questão01/ExercicioNumero.cs
@@ -11,12 +11,12 @@ namespace TrabalhoOrientacaoObjetos01.Questão01
         public void Executar()
         {
 
-            //Numero numero = new Numero();
+            Numero numero = new Numero();
 
-            //Console.WriteLine("Informe um valor: ");
-            //numero.Valor = Convert.ToDouble(Console.ReadLine().ToString());
+            Console.WriteLine("Informe um valor: ");
+            numero.Valor = Convert.ToDouble(Console.ReadLine().ToString());
 
-
+            
 
 
         }
